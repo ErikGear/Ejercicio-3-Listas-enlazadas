6fd65e07dba42e6c2787f78e1e7ef5cfb921827a
ErikGear <erikgalindo23@aragon.unam.mx>
2023-10-15 15:50:55 -0600

    Agregando implemntación de lista doblemente enlazada

---
+++ Ejercicio2/main.js
@@ -0,0 +1,93 @@
+class ListaDoble {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+
+    this.size = 0;
+  }
+
+  get getSize() {
+    return this.size;
+  }
+
+  get isEmpty() {
+    return this.size === 0;
+  }
+
+  insertarNodo(nodo) {
+    const nuevoNodo = new NodoDoble(nodo);
+    if (this.head === null) {
+      this.head = nuevoNodo;
+      this.tail = nuevoNodo;
+    } else {
+      this.tail._next = nuevoNodo;
+      nuevoNodo._previous = this.tail;
+      this.tail = nuevoNodo;
+    }
+
+    this.size++;
+  }
+
+  recorrerLista() {
+    let tmp = this.head;
+
+    while (tmp !== null) {
+      console.log(`${tmp._data}`);
+      tmp = tmp._next;
+    }
+  }
+
+  recorridoInverso() {
+    let tmp = this.tail;
+
+    while (tmp !== null) {
+      console.log(`${tmp._data}`);
+      tmp = tmp._previous;
+    }
+  }
+
+  eliminarNodo(nodo) {
+    if (this.size === 0) {
+      throw "Error, Lista sin elementos";
+    }
+
+    //cuando el nodo es la cabeza HEAD
+    if (this.head._data === nodo) {
+      if (this.size === 1) {
+        this.head = null;
+        this.tail = null;
+      } else {
+        this.head = this.head._next;
+        this.head._previous = null;
+      }
+      this.size--;
+
+      //cuando el nodo esta en la cola
+    } else if (this.tail._data === nodo) {
+      this.tail = this.tail._previous;
+      this.tail._next = null;
+      this.size--;
+    } else {
+      //cuando el nodo esta en el medio de la lista
+      let tmp = this.head;
+
+      while (tmp._next !== null) {
+        if (tmp._next._data === nodo) {
+          tmp._next = tmp._next._next;
+          tmp._next._previous = tmp;
+          this.size--;
+          break;
+        }
+        tmp = tmp._next;
+      }
+    }
+  }
+
+  clear() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+}
+
+const listaDoble = new ListaDoble();
